0a67d2905aa0f463a27b38382ef5c8ea7eb2cda3
Ismail <ghvh8176@gmail.com>
2025-11-05 12:53:08 -0600

    finish the project

--- Professional Projects/1. caesar_clipher.py
+++ Professional Projects/1. caesar_clipher.py
@@ -1,14 +1,22 @@
-def caesar(text, shift):
+def caesar(text, shift, encrypt=True):
+
     if not isinstance(shift, int):
         return 'Shift must be an integer value.'
 
-    if shift < 1:
-        return "Shift must be a positive integer."
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
 
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    
+    if not encrypt:
+       shift = -shift
+
+  
+    
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(translation_table)
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
 
 
 encrypted_text = caesar('freeCodeCamp', 3)
