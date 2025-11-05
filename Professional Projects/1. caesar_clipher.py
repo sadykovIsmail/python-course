cb47ea143dec3f5f78b92e54342f8f40f6bd8a8b
Ismail <ghvh8176@gmail.com>
2025-11-05 10:17:58 -0600

    make the parameters

--- Professional Projects/1. caesar_clipher.py
+++ Professional Projects/1. caesar_clipher.py
@@ -1,7 +1,6 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = 'hello world'
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)
+def caesar(text, shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
