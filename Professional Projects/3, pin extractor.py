a2b2fe80db81cdf9796b120eb6fb822c3729637a
Ismail <ghvh8176@gmail.com>
2025-11-07 12:32:09 -0600

    add else

--- Professional Projects/3, pin extractor.py
+++ Professional Projects/3, pin extractor.py
@@ -5,11 +5,13 @@ def pin_extractor(poem):
         words = line.split()
         if len(words) > line_index:
            secret_code += str(len(words[line_index]))
+        else:
+            secret_code += '0'
     return secret_code
 
 poem = """Stars and the moon
 shine in the sky
-white and bright
+
 until the end of the night"""
 
 print(pin_extractor(poem))
