30978c3e54f4064ad58b3c659efaeb054f8ed2f6
Ismail <ghvh8176@gmail.com>
2025-11-07 15:57:01 -0600

    optimized version

--- Professional Projects/4. numbet pattern generator.py
+++ Professional Projects/4. numbet pattern generator.py
@@ -9,3 +9,18 @@ def number_pattern(n):
     return numbers.strip()
 
 print(number_pattern(4))
+
+
+# Optimized version
+
+"""
+    
+    def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    elif n < 1:
+        return "Argument must be an integer greater than 0."
+    
+    return " ".join(str(num) for num in range(1, n + 1))
+
+    """
