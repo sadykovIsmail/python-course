d9fb50d4a28b69d72671ab4eaeaa997bfb6722e9
Ismail <ghvh8176@gmail.com>
2025-11-07 12:25:52 -0600

    update to return code

--- Professional Projects/3, pin extractor.py
+++ Professional Projects/3, pin extractor.py
@@ -2,9 +2,10 @@ def pin_extractor(poem):
     secret_code = ''
     lines = poem.split('\n')
     for line_index, line in enumerate(lines):
-        print(line)
         words = line.split()
-        print(line_index, line)
+       
+        secret_code += str(len(words[line_index]))
+    return secret_code
 
 poem = """Stars and the moon
 shine in the sky
