603943040a99f58b45923fc9705f42bfa8996bfb
Ismail <ghvh8176@gmail.com>
2025-11-08 11:08:41 -0600

    make unique

--- Professional Projects/5. medical data validation.py
+++ Professional Projects/5. medical data validation.py
@@ -30,21 +30,37 @@ medical_records = [
         'diagnosis': 'Chronic Back Pain',
         'medications': ['Ibuprofen', 'Physical Therapy'],
         'last_visit_id': 'V2304',
-    }, 
-    "testing"
+    }
 ]
+
+
 def validate(data):
     is_sequence = isinstance(data, (list, tuple))
+
     if not is_sequence:
         print('Invalid format: expected a list or tuple.')
         return False
+        
     is_invalid = False
+    key_set = set(
+        ['patient_id', 'age', 'gender', 'diagnosis', 'medications', 'last_visit_id']
+    )
+
     for index, dictionary in enumerate(data):
         if not isinstance(dictionary, dict):
-            print(f"Invalid format: expected a dictionary at position {index}.")
+            print(f'Invalid format: expected a dictionary at position {index}.')
             is_invalid = True
+
+        if set(dictionary.keys()) != key_set:
+            print(
+                f'Invalid format: {dictionary} at position {index} has missing and/or invalid keys.'
+            )
+            is_invalid = True
+
     if is_invalid:
         return False
-    print('Valid format')
+    print('Valid format.')
     return True
-            
+
+
+validate(medical_records)
