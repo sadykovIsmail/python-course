47cec5c44f250716e9fab1953a8d5d1e824bbd1f
Ismail <ghvh8176@gmail.com>
2025-11-08 10:48:06 -0600

    validate function

--- Professional Projects/5. medical data validation.py
+++ Professional Projects/5. medical data validation.py
@@ -32,3 +32,14 @@ medical_records = [
         'last_visit_id': 'V2304',
     }
 ]
+def validate(data):
+    is_sequence = isinstance(data, (list, tuple))
+    if not is_sequence:
+        print('Invalid format: expected a list or tuple.')
+        return False
+    is_invalid = False
+    for index, dictionary in enumerate(data):
+        if not isinstance(dictionary, dict):
+            print(f"Invalid format: expected a dictionary at position {index}.")
+            is_invalid = True
+            
