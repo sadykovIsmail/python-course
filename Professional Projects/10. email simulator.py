2bea8781515215eb4b4924c2a297e214a699f922
Ismail <ghvh8176@gmail.com>
2025-11-09 16:43:29 -0600

    test

--- Professional Projects/10. email simulator.py
+++ Professional Projects/10. email simulator.py
@@ -1,11 +1,17 @@
 class Email:
-    def __init__(self, sender, receiver, subject, body, read=False):
+    def __init__(self, sender, receiver, subject, body):
         self.sender = sender
         self.receiver = receiver
         self.subject = subject
         self.body = body
         self.read = False
-        
-email_obj = Email("alice@example.com", "bob@example.com", "Hello", "Hi Bob!")
+
+    def mark_as_read(self):
+        self.read = True
+
+email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!')
 print(email_obj.sender)
 print(email_obj.subject)
+print(email_obj.read)
+email_obj.mark_as_read()
+print(email_obj.read)
