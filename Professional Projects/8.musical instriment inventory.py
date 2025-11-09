2ce4a8ca66fc2c5fe7136eaad8773819cf24debf
Ismail <ghvh8176@gmail.com>
2025-11-09 12:59:44 -0600

    add abother attribute

--- Professional Projects/8.musical instriment inventory.py
+++ Professional Projects/8.musical instriment inventory.py
@@ -2,11 +2,10 @@ class MusicalInstrument:
     def __init__(self, name, instrument_type):
         self.name  = name
         self.instrument_type = instrument_type
+        
+    def play(self):
+        pass
 
 instrument_1 = MusicalInstrument("Oboe", "woodwind")
 instrument_2 = MusicalInstrument("Trumpet", "brass")
 
-print(instrument_1.name)
-print(instrument_1.instrument_type)
-print(instrument_2.name)
-print(instrument_2.instrument_type)
