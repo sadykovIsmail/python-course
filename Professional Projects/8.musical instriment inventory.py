ebf1d46999632ffdf05ac94ef5e206ffccf172a3
Ismail <ghvh8176@gmail.com>
2025-11-09 12:53:06 -0600

    create and check class

---
+++ Professional Projects/8.musical instriment inventory.py
@@ -0,0 +1,7 @@
+class MusicalInstrument:
+    def __init__(self, name, instrument_type):
+        self.name  = name
+        self.instrument_type = instrument_type
+
+instrument_1 = MusicalInstrument("Oboe", "woodwind")
+    
