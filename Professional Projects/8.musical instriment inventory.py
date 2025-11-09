b813c8bcc2b31189295720676d7608b58cf03d34
Ismail <ghvh8176@gmail.com>
2025-11-09 12:55:58 -0600

    add abother instance

--- Professional Projects/8.musical instriment inventory.py
+++ Professional Projects/8.musical instriment inventory.py
@@ -4,4 +4,4 @@ class MusicalInstrument:
         self.instrument_type = instrument_type
 
 instrument_1 = MusicalInstrument("Oboe", "woodwind")
-    
+instrument_2 = MusicalInstrument("Trumpet", "brass")
