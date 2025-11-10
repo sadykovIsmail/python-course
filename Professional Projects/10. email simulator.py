c0843fba9a1a2975abcf70993b82b16d7bc3ad64
Ismail <ghvh8176@gmail.com>
2025-11-09 18:33:20 -0600

    tested

--- Professional Projects/10. email simulator.py
+++ Professional Projects/10. email simulator.py
@@ -25,9 +25,3 @@ class Inbox:
     def receive_email(self, email):
         self.emails.append(email)
 
-alice = User("Alice")
-bob = User("Bob")
-
-alice.send_email(bob, "Hello", "Hi Bob, how are you?")
-
-print(len(bob.inbox.emails))
