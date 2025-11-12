a70f8c8de1a3eeb8a538c93d0d75d2fca568a656
Ismail <ghvh8176@gmail.com>
2025-11-12 12:49:14 -0600

    iterate for all emails

--- Professional Projects/10. email simulator.py
+++ Professional Projects/10. email simulator.py
@@ -41,4 +41,12 @@ class Inbox:
         if not self.emails:
             print('Your inbox is empty.\n')
             return
+
+        print("\nYour Emails:")
+
+        for i, email in enumerate(self.emails, start=1):
+            print(f"{i}. {email}")
+
+
+        
         
