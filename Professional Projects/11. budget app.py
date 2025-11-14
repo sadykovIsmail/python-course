f6295f13d684e626651250bd94f415d94caec636
Ismail <ghvh8176@gmail.com>
2025-11-14 02:33:54 -0600

    update

--- Professional Projects/11. budget app.py
+++ Professional Projects/11. budget app.py
@@ -8,22 +8,34 @@ class Category:
         self.ledger = []
         
     
-    def deposit(self, amount1, description=""):
+    def deposit(self, amount, description=""):
         
-        self.ledger.append({'amount': amount1, 'description': description})
+        self.ledger.append({'amount': amount, 'description': description})
         # return print(self.ledger)
 
     def withdraw(self, amount, description=""):
-           
-            
+    # Calculate current balance
+        balance = sum(item["amount"] for item in self.ledger)
+
+    # If not enough money → return False
+        if amount > balance:
+            return False
+
+    # Otherwise add a withdrawal entry (negative amount)
+        self.ledger.append({"amount": -amount, "description": description})
+        return True
+
         
-        return print(self.ledger[0][amount1])
+        
+        
+        
 def create_spend_chart(categories):
     pass
 
 hello = Category(0)
 hello.deposit(35)
-
+hello.withdraw(3)
+print(hello.ledger)
 # hello.withdraw(30)
 
 
