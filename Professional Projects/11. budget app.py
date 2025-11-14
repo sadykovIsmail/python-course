c3177d4a72b520a60890ceaddfe131590c5fa140
Ismail <ghvh8176@gmail.com>
2025-11-13 19:24:14 -0600

    update

---
+++ Professional Projects/11. budget app.py
@@ -0,0 +1,31 @@
+
+
+class Category:
+    
+    
+    def __init__(self, ledger):
+        self.ledger = ledger
+        self.ledger = []
+        
+    
+    def deposit(self, amount1, description=""):
+        
+        self.ledger.append({'amount': amount1, 'description': description})
+        # return print(self.ledger)
+
+    def withdraw(self, amount, description=""):
+           
+            
+        
+        return print(self.ledger[0][amount1])
+def create_spend_chart(categories):
+    pass
+
+hello = Category(0)
+hello.deposit(35)
+
+# hello.withdraw(30)
+
+
+
+
---
+++ Professional Projects/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+hello.withdraw(30)
