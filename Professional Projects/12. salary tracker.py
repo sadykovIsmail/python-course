212a807e9d1fbea3ae26b16a8eff3866d841aab7
Ismail <ghvh8176@gmail.com>
2025-11-14 20:52:22 -0600

    same with level

--- Professional Projects/12. salary tracker.py
+++ Professional Projects/12. salary tracker.py
@@ -2,14 +2,17 @@ class Employee:
     def __init__(self, name, level):
         self._name = name
         self._level = level
-
     def __str__(self):
         return f'{self._name}: {self._level}'
     @property
     def name(self):
         return self._name
 
-    
+    @property
+    def level(self):
+        return self._level
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
 print(charlie_brown.name)
+print(charlie_brown.level)
