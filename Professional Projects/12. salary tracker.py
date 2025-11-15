625157a99eeb0ca41288a3c61da08f8c8bc0c41b
Ismail <ghvh8176@gmail.com>
2025-11-14 21:12:13 -0600

    add class attribute

--- Professional Projects/12. salary tracker.py
+++ Professional Projects/12. salary tracker.py
@@ -1,10 +1,16 @@
 class Employee:
+    _base_salaries = {
+    'trainee': 1000,
+    'junior': 2000,
+    'mid-level': 3000,
+    'senior': 4000
+}
     def __init__(self, name, level):
-        if not isinstance(name, str) or not isinstance(level, str):
+        if not (isinstance(name, str) and isinstance(level, str)):
             raise TypeError("'name' and 'level' attribute must be of type 'str'.")
-            
         self._name = name
         self._level = level
+
     def __str__(self):
         return f'{self.name}: {self.level}'
 
