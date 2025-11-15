7018d93042793591cbb61233d5c20c41438cf7f9
Ismail <ghvh8176@gmail.com>
2025-11-14 21:03:56 -0600

    add validation for str

--- Professional Projects/12. salary tracker.py
+++ Professional Projects/12. salary tracker.py
@@ -1,8 +1,10 @@
 class Employee:
     def __init__(self, name, level):
+        if not isinstance(name, str) or not isinstance(level, str):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
+            
         self._name = name
         self._level = level
-
     def __str__(self):
         return f'{self.name}: {self.level}'
 
@@ -16,7 +18,6 @@ class Employee:
     @property
     def level(self):
         return self._level
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
-
-print(repr(charlie_brown))
