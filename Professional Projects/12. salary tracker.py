607df8306e2fbf57472851e73fe1d6e1d1eb7767
Ismail <ghvh8176@gmail.com>
2025-11-14 20:48:14 -0600

    add str method

--- Professional Projects/12. salary tracker.py
+++ Professional Projects/12. salary tracker.py
@@ -1,6 +1,10 @@
 class Employee:
     def __init__(self, name, level):
-        self.name = name
-        self.level = level
+        self._name = name
+        self._level = level
+        
+    def __str__(self):
+        return f"{self._name}: {self._level}"
 
 charlie_brown = Employee('Charlie Brown', 'trainee')
+print(charlie_brown)
