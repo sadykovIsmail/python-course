3850432543279c393db99a4ca8369d2a0d1f160e
Ismail <ghvh8176@gmail.com>
2025-12-05 21:10:32 -0600

    decorator

--- Professional Projects/13. game character.py
+++ Professional Projects/13. game character.py
@@ -21,7 +21,19 @@ class GameCharacter:
         elif new_health > 100: 
             self._health = 100
         else:
-            
             self._health = new_health
-            
+    @property       
+    def mana(self):
+        return self._mana
     
+    @mana.setter
+    def mana(self, new_mana):
+        if new_mana < 0:
+            self._mana = 0
+            
+        elif new_mana > 50:
+            self._mana = 50 
+        else:
+            self._mana = new_mana
+            
+            
