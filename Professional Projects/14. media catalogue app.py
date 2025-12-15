0d0115bf3d8408cc2e9083f6c0d769233803718c
Ismail <ghvh8176@gmail.com>
2025-12-15 07:13:14 -0600

    create media catalogue app

---
+++ Professional Projects/14. media catalogue app.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self, title, year, director, duration):
+        self.title = title
+        self.year = year
+        self.director = director
+        self.duration = duration
+        
