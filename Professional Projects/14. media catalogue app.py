9e69d248a7cd0098594e5fe3e478df565e472e52
Ismail <ghvh8176@gmail.com>
2025-12-15 07:31:19 -0600

    add validation for title

--- Professional Projects/14. media catalogue app.py
+++ Professional Projects/14. media catalogue app.py
@@ -1,14 +1,13 @@
 class Movie:
     def __init__(self, title, year, director, duration):
+        if title == "" or title.strip() == "" :
+            raise ValueError("Title cannot be empty")
         self.title = title
         self.year = year
         self.director = director
         self.duration = duration
-        
     def __str__(self):
-        return f"{self.title} ({self.year}) - {self.duration} min, {self.director}"
-    
-    
-movie1 = Movie('harry', 2023, 'me', 1)
+        return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
 
+movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
 print(movie1)
