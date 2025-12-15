8e77c5e800d3311761267453c3d10204a177319d
Ismail <ghvh8176@gmail.com>
2025-12-15 12:20:47 -0600

    media error

--- Professional Projects/14. media catalogue app.py
+++ Professional Projects/14. media catalogue app.py
@@ -1,3 +1,10 @@
+class MediaError(Exception):
+    """Custom exception for media-related errors."""
+        
+    def __init__(self, message, obj):
+        super().__init__(message)
+        self.obj = obj
+
 class Movie:
     """Parent class representing a movie."""
     
