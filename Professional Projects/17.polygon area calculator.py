80af1c1105b3fa9de541a8f67e686be37fbbb5a7
Ismail <ghvh8176@gmail.com>
2025-12-16 11:21:30 -0600

    polygon area calculator

---
+++ Professional Projects/17.polygon area calculator.py
@@ -0,0 +1,29 @@
+class Rectangle:
+    def __init__(self):
+        
+        self.width = 0
+        self.height = 0
+    
+    #setter
+    def set_width(self, value: int):
+        self.width = value
+        return f"Width set to: {self.width}"
+        
+    #setter
+    def set_height(self, value: int):
+        self.height = value
+        return f"Width set to: {self.height}"
+
+        
+    def get_area(self):
+        area = self.width * self.height
+        return area
+class Square(Rectangle):
+    pass
+    
+    
+rectangle = Rectangle()
+
+print(rectangle.set_width(3))
+print(rectangle.set_height(2))
+print(rectangle.get_area())
