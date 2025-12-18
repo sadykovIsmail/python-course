511c44d1ba01e8d54a39be465a17e34be759ccf2
Ismail <ghvh8176@gmail.com>
2025-12-18 13:34:08 -0600

    create method to keep track of emptines

--- Professional Projects/18. linked list.py
+++ Professional Projects/18. linked list.py
@@ -2,3 +2,12 @@ class LinkedList:
     class Node:
         def __init__(self, element):
             self.element = element
+            self.next = None
+            
+    def __init__(self):
+        self.length = 0
+        self.head = None
+
+    def is_empty(self):
+        return self.length == 0
+
