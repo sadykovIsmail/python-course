c2a7bc9d8d837cbc56c222f0a249d1d1297ee57b
Ismail <ghvh8176@gmail.com>
2025-12-20 13:30:25 -0600

    complete project

--- Professional Projects/22. merge sort.py
+++ Professional Projects/22. merge sort.py
@@ -1,4 +1,6 @@
 def merge_sort(array):
+    if len(array) <= 1:
+        return
     
     middle_point = len(array) // 2
     left_part = array[:middle_point]
@@ -14,5 +16,27 @@ def merge_sort(array):
     while left_array_index < len(left_part) and right_array_index < len(right_part):
         if left_part[left_array_index] < right_part[right_array_index]:
             array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
         else:
             array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+    
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: ')
+    print(numbers)
