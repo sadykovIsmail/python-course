618e6e62ef614072cdc31409c887aa4d48610ea1
Ismail <ghvh8176@gmail.com>
2026-01-20 16:43:26 -0600

    delete

--- django/app/polls/urls.py
+++
@@ -1,8 +0,0 @@
-# polls/polls/urls.py (project-level)
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),  # include your app's urls
-]
